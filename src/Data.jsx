d80d9552981bda2c32dfc42a5e4a549712e4dca3
foferys <foferys@gmail.com>
2024-05-28 17:26:55 +0200

    primo commit

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name: "Katia Falletti",
+        email: "katia@gmail.com",
+        id: 1
+    },
+    {
+        name: "Gianpiero Ferraor",
+        email: "gian@gmail.com",
+        id: 2
+    },
+    {
+        name: "Marco scorzo",
+        email: "marco@gmail.com",
+        id: 3
+    }
+]
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+
+/* user reducer */
+const store = configureStore({
+  reducer: {
+    // user:  
+  }
+})
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+)
